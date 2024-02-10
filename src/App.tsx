e91fa67f764161d810731e355fc28a9bba8601b6
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2024-02-10 11:27:39 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
-import { useEthereum, useConnect, useAuthCore } from '@particle-network/auth-core-modal';
 import { BerachainArtio } from '@particle-network/chains';
-
+import { AAWrapProvider, SendTransactionMode, SmartAccount } from '@particle-network/aa';
+import { useEthereum, useConnect, useAuthCore } from '@particle-network/auth-core-modal';
 import { ethers } from 'ethers';
 import { notification } from 'antd';
 
@@ -15,7 +14,16 @@ const App = () => {
 
   const [balance, setBalance] = useState(null);
 
-  const customProvider = new ethers.providers.Web3Provider(provider, "any");
+  const smartAccount = new SmartAccount(provider, {
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    clientKey: process.env.REACT_APP_CLIENT_KEY,
+    appId: process.env.REACT_APP_APP_ID,
+    aaOptions: {
+      simple: [{ chainId: BerachainArtio.id, version: '1.0.0' }],
+    },
+  });
+
+  const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount, SendTransactionMode.Gasless), "any");
 
   useEffect(() => {
     if (userInfo) {
@@ -23,22 +31,22 @@ const App = () => {
     }
   }, [userInfo]);
 
-  const fetchBalance = async () => {
-    const balanceResponse = await customProvider.getBalance(await customProvider.getSigner().getAddress());
-
+  const fetchBalance = async() => {
+    const address = await smartAccount.getAddress();
+    const balanceResponse = await customProvider.getBalance(address);
     setBalance(ethers.utils.formatEther(balanceResponse));
-  }
+  };
 
   const handleLogin = async (authType) => {
     if (!userInfo) {
       await connect({
         socialType: authType,
-        chain: BerachainArtio,
+        chain: BerachainArtio
       });
     }
   };
 
-  const executeTx = async () => {
+  const executeUserOp = async () => {
     const signer = customProvider.getSigner();
 
     const tx = {
@@ -50,30 +58,30 @@ const App = () => {
     const txReceipt = await txResponse.wait();
 
     notification.success({
-      message: `Transaction Successful`,
+      message: "Transaction Successful",
       description: (
-          <div>
-            Transaction Hash: <a href={`https://artio.beratrail.io/tx/${txReceipt.transactionHash}`} target="_blank" rel="noopener noreferrer">{txReceipt.transactionHash}</a>
-          </div>
-        )
+        <div>
+          Transaction Hash: <a href={`https://artio.beratrail.io/tx/${txReceipt.transactionHash}`} target="_blank" rel="noopener noreferrer">{txReceipt.transactionHash}</a>
+        </div>
+      )
     });
   };
 
-  const executeTxHONEY = async () => {
+  const executeUserOpHONEY = async () => {
     const signer = customProvider.getSigner();
-    
+
     const tokenContract = new ethers.Contract('0x7EeCA4205fF31f947EdBd49195a7A88E6A91161B', ["function transfer(address to, uint256 amount)"], signer);
-    
+
     const txResponse = await tokenContract.transfer('0x000000000000000000000000000000000000dEaD', ethers.utils.parseEther('1'));
     const txReceipt = await txResponse.wait();
-    
+
     notification.success({
-      message: `Transaction Successful`,
+      message: "Transaction Successful",
       description: (
-          <div>
-            Transaction Hash: <a href={`https://artio.beratrail.io/tx/${txReceipt.transactionHash}`} target="_blank" rel="noopener noreferrer">{txReceipt.transactionHash}</a>
-          </div>
-        )
+        <div>
+          Transaction Hash: <a href={`https://artio.beratrail.io/tx/${txReceipt.transactionHash}`} target="_blank" rel="noopener noreferrer">{txReceipt.transactionHash}</a>
+        </div>
+      )
     });
   };
 
@@ -101,9 +109,9 @@ const App = () => {
         <div className="profile-card">
           <h2>{userInfo.name}</h2>
           <div className="balance-section">
-            <small>{balance} ETH</small>
-            <button className="sign-message-button" onClick={executeTx}>Execute Transaction</button>
-            <button className="sign-message-button honey" onClick={executeTxHONEY}>Execute Transaction ($HONEY)</button>
+            <small>{balance} BERA</small>
+            <button className="sign-message-button" onClick={executeUserOp}>Execute Transaction</button>
+            <button className="sign-message-button honey" onClick={executeUserOpHONEY}>Execute Transaction ($HONEY)</button>
             <button className="disconnect-button" onClick={() => disconnect()}>Logout</button>
           </div>
         </div>
